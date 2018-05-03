b6e014401b0b510bd5db5f5d7338684e664959f2
eduxstad <eduxstad@gmail.com>
2018-05-03 15:23:52 -0500

    add #4 2017

---
+++ src/number42017/Position.java
@@ -0,0 +1,15 @@
+package number42017;
+
+public class Position {
+
+	int row;
+	int column;
+	
+	public Position(int r, int c) {
+		this.row = r;
+		this.column = c;
+	}
+	
+	
+
+}
---
+++ src/number42017/Successors.java
@@ -0,0 +1,21 @@
+package number42017;
+
+public class Successors {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+	
+	public static Position findPosition(int num, int[][] intArr) {
+		
+		// WRITE YOUR CODE HERE (PART 1)
+		return null;
+	}
+	
+	public static Position[][] getSuccessorArray(int[][] intArr) {
+		
+		// WRITE YOUR CODE HERE (PART 2)
+		return null;
+	}
+
+}
